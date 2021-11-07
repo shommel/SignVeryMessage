5e47123ee14c55216e305289e458b973f515bd4b
Spencer Hommel <spencer.hommel@avantibank.com>
2021-11-07 16:01:41 -0500

    [REACT] beginnings of trezor xpub + QR code functionalities. Adding Trezor-Connect and QRCode libraries to package.json

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import TrezorConnect from 'trezor-connect';
+import QRCode from 'qrcode.react';
+import React, {useState} from "react";
+
+function initTrezor() {
+  TrezorConnect.init({
+  // connectSrc: 'https://localhost:8088/',
+  lazyLoad: false,
+    manifest: {
+      email: 'developer@xyz.com',
+      appUrl: 'http://your.application.com',
+    },
+  }).then(() => {
+      console.log('trezor-connect', 'TrezorConnect is ready!');
+  }).catch(error => {
+      console.log('trezor-connect', 'TrezorConnect init error:' + error);
+  });
+}
 
 function App() {
+
+  const [showImage, setShowImage] = useState(false);
+  const [xpub, setxPub] = useState("")
+  initTrezor();
+  
+  const getPublicKey = () => {
+      TrezorConnect.getPublicKey({
+        path: "m/49'/0'/0'",
+        coin: "btc"
+      }).then(resp => {
+          if (resp.success) {
+            console.log(resp.payload.xpub);
+            setShowImage(true);
+            setxPub(resp.payload.xpub)
+          }
+      })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Trezor Integration</h1>
+        <p>Please plug-in your Trezor!</p>
+        <button onClick={getPublicKey}>Get xpub</button>
+        {showImage && <QRCode className="xPubQrCode" value={xpub}/>} 
       </header>
     </div>
   );
